d2cc08afffcb631d73fe8cae4aad5c72e4a8cb29
Michael Zapatka <michael@evesaddiction.com>
2019-03-09 23:13:44 -0500

    Updating SQLs

--- sql/services_content_3.sql
+++ sql/services_content_3.sql
@@ -1,2 +1,4 @@
-ALTER TABLE services_content ADD COLUMN services_content_idx int(2) not null;
-ALTER TABLE services_content DROP PRIMARY KEY, ADD PRIMARY KEY(services_content_id, services_content_page, services_content_type, services_content_idx);
+ALTER TABLE services_content DROP PRIMARY KEY, ADD PRIMARY KEY(services_content_id, services_content_type, services_content_idx);
+ALTER TABLE services_content DROP COLUMN services_content_page;
+ALTER TABLE services_content ADD COLUMN services_content_site_id int(10) not null;
+ALTER TABLE services_content DROP PRIMARY KEY, ADD PRIMARY KEY(services_content_id, services_content_site_id, services_content_type, services_content_idx);
